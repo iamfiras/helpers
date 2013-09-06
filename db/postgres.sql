e8758c7e08f215b8527866cd74e1556adcc63190
Firas Bessadok <firas.bess@gmail.com>
2013-09-06 22:59:56 +0200

    removing tabs

--- db/postgres.sql
+++ db/postgres.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS example1 (
-	id				SERIAL PRIMARY KEY, -- SERIAL is a "shortcut" for an integer auto_increment column
-	name			varchar(255) NOT NULL,
-	category	integer REFERENCES example2(id) -- REFERENCES means foreign key 
+	id SERIAL PRIMARY KEY, -- SERIAL is a "shortcut" for an integer auto_increment column
+	name varchar(255) NOT NULL,
+	category integer REFERENCES example2(id) -- REFERENCES means foreign key 
 );
